5462ee323e8e6cda44f3327add26bc01ff7a52dc
Javier Aguirre <contacto@javaguirre.net>
2012-04-03 22:05:28 +0200

    Added export module

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ import csv
 app = Flask(__name__)
 FILE_PATH = '/tmp/image'
 
+
+#TODO descargar como CSV
+#Ponerlo bonito en la web
+#Meterle el login
+#Meterle el plugin de tablesorter
+#TODO Controlar el tamaño de los archivos
 @app.route("/", methods=['GET', 'POST'])
 def index():
     colours = ''
@@ -12,16 +18,23 @@ def index():
         f = request.files['image']
         f.save(FILE_PATH)
         im = Image.open('/tmp/image')
-#TODO descargar como CSV
-#Ponerlo bonito en la web
-#Meterle el login
-#Meterle el plugin de tablesorter
         colours = im.getcolors(im.size[0]*im.size[1])
-        color_result = csv.writer(open('result.csv', 'wb'), delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for colour in colours:
-            color_result.writerow(colour)
 
     return render_template('index.html', colours=colours)
+
+
+@app.route("/export")
+def export():
+    im = Image.open('/tmp/image')
+    colours = im.getcolors(im.size[0]*im.size[1])
+    f = open('result.csv', 'wb')
+    colour_result = csv.writer(f, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    for colour in colours:
+        colour_result.writerow(colour)
+
+    return f
+
 
 if __name__ == "__main__":
     app.debug = True
