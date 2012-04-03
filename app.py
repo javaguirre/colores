e65c140cccdd68f63acccbc0a5ad9aaf5e353aeb
Javier Aguirre <contacto@javaguirre.net>
2012-04-03 21:53:17 +0200

    Improving interface

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 import Image
+import csv
 
 app = Flask(__name__)
 FILE_PATH = '/tmp/image'
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    result = ''
+    colours = ''
     if request.method == 'POST':
         f = request.files['image']
         f.save(FILE_PATH)
@@ -14,8 +15,13 @@ def index():
 #TODO descargar como CSV
 #Ponerlo bonito en la web
 #Meterle el login
-        result = im.getcolors(im.size[0]*im.size[1])
-    return render_template('index.html', result=result)
+#Meterle el plugin de tablesorter
+        colours = im.getcolors(im.size[0]*im.size[1])
+        color_result = csv.writer(open('result.csv', 'wb'), delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for colour in colours:
+            color_result.writerow(colour)
+
+    return render_template('index.html', colours=colours)
 
 if __name__ == "__main__":
     app.debug = True
